b0850ebfb07e4da019a46f10bfd40df56bd46ae3
Lisa Scott <lrscot@aol.com>
2019-02-15 01:04:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,42 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+
+people.each do |k,v|
+if v == "Winner"
+full_name = people["name"]
+return full_name.split(' ').first
+end
+end
+end
 end
 
+
+
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
 end
 
 def get_occupation(data, hometown)
